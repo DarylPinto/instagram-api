78c348adb77ddc223c5dff779481bb68cb88855e
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-10-07 10:27:27 -0400

    Improve error logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const parse = require("./utilities/scrape.js");
+const logError = require("./utilities/logError.js");
 
 const app = express();
 const port = 3000;
@@ -20,7 +21,8 @@ app.get("/api/:username", async (req, res) => {
 
 		// If there's a server-side exception
 		// Log the stack and respond with a 500 error
-		console.error(`[${new Date().toUTCString()}]: ${err.stack}`);
+		// console.error(`[${new Date().toUTCString()}]: ${err.stack}`);
+		logError(err, `Attempted to get profile for ${username}`);
 		return res.status(500).json({
 			status: 500,
 			message: err.message
---
+++ utilities/logError.js
@@ -0,0 +1,10 @@
+const tz = { timeZone: "America/New_York" };
+
+module.exports = (err, info = "No additional info") => {
+	console.error(`
+		===============================
+		Time: ${new Date().toLocaleString("en-CA", tz)}
+		Info: ${info}
+		-- Stack Trace Below --\n${err.stack}
+	`.trim().replace(/\t/g, ""));
+};
