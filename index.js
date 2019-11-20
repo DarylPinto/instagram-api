9ff4ca9012e605f848891ad80e7d9c47355a1e4f
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-11-20 14:54:24 -0500

    Use const where appropriate, be slightly more explicit in error logging

--- index.js
+++ index.js
@@ -16,19 +16,18 @@ const limit = rateLimit({
 
 app.get("/:username", limit, async (req, res) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	let username;
 
 	try {
 		// Get username from request
-		username = req.params.username;
+		const username = req.params.username;
 		// Check if data is already cached
-		let cached = cache.load(username);
+		const cachedData = cache.load(username);
 
 		// If it's already cached
 		// check if cache is fresh enough (according to config.json)
 		// If so, send the cached data instead of scraping instagram's live URL
-		if (cached) {
-			const { status, response, time } = cached;
+		if (cachedData !== null) {
+			const { status, response, time } = cachedData;
 			const cacheAge = Math.floor((new Date() - new Date(time)) / 1000 / 60);
 			if (cacheAge < config["cache_duration_minutes"]) {
 				return res.status(status).json(JSON.parse(response));
@@ -37,12 +36,12 @@ app.get("/:username", limit, async (req, res) => {
 
 		// If there's no cache or if the cached data is too old (according to config.json)
 		// scrape instagram's live URL
-		const data = await scrape(username);
+		let data = await scrape(username);
 
 		// If there's no data returned from the scraper
 		// respond with 404
 		if (data === null) {
-			let data = { status: 404, message: "Instagram User Not Found" };
+			data = { status: 404, message: "Instagram User Not Found" };
 			cache.save(username, 404, data);
 			return res.status(404).json(data);
 		}
@@ -52,7 +51,7 @@ app.get("/:username", limit, async (req, res) => {
 		return res.send(data);
 	} catch (err) {
 		// Respond with a 500 error if there's a server side exception
-		let data = { status: 500, message: "Internal Server Error" };
+		const data = { status: 500, message: "Internal Server Error" };
 		logError(err, err.message);
 		return res.status(500).json(data);
 	}
--- utilities/log-error.js
+++ utilities/log-error.js
@@ -3,6 +3,7 @@ const tz = { timeZone: "America/New_York" };
 module.exports = (err, info = "No additional info") => {
 	console.error(`
 		===============================
+		[ ERROR ]
 		Time: ${new Date().toLocaleString("en-CA", tz)}
 		Info: ${info}
 		-- Stack Trace Below --
