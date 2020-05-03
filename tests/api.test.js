d75e71fb57ad56d02675aa96e2256741e858eb40
Daryl Pinto <daryl.j.pinto@gmail.com>
2020-05-03 14:17:42 -0400

    test: format code and adjust test names

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,53 +4,53 @@ const nowApiBaseUri = "https://insta-api.now.sh/api";
 
 jest.setTimeout(20000);
 
-// Ensures API is running on localhost
-test("Is live", async done => {
-	const res = await fetch(nowUri);
-	expect(res.text).toBeDefined();
-	done();
-});
+describe("The Prod API", () => {
+	it("is live", async (done) => {
+		const res = await fetch(nowUri);
+		const html = await res.text();	
+		expect(html).toBeDefined();
+		done();
+	});
 
-// Ensures API properly responds to user requests
-test("Fetches Instagram users", async done => {
-	const usernames = ["cristiano", "arianagrande", "therock", "leomessi"];
-	let responses = await Promise.all(
-		usernames.map(username => fetch(`${nowApiBaseUri}/${username}`))
-	);
+	it("can fetch valid Instagram users", async (done) => {
+		const usernames = ["cristiano", "arianagrande", "therock", "leomessi"];
+		let responses = await Promise.all(
+			usernames.map((username) => fetch(`${nowApiBaseUri}/${username}`))
+		);
 
-	responses = await Promise.all(responses.map(res => res.json()));
+		responses = await Promise.all(responses.map((res) => res.json()));
 
-	responses = responses.map(res => res.username);
+		responses = responses.map((res) => res.username);
 
-	expect(JSON.stringify(responses)).toBe(JSON.stringify(usernames));
+		expect(JSON.stringify(responses)).toBe(JSON.stringify(usernames));
 
-	done();
-});
+		done();
+	});
 
-// Ensures API responds with 'Not Found error' for
-// invalid usernames/non-existent users
-test("404s invalid usernames", async done => {
-	const usernames = [
-		"developer",
-		"explore",
-		"about",
-		"DEMO_USERNAME_TOO_LONG_TO_BE_REAL_TESTING_TESTING_123"
-	];
+	it("404s invalid usernames", async (done) => {
+		const usernames = [
+			"developer",
+			"explore",
+			"about",
+			"DEMO_USERNAME_TOO_LONG_TO_BE_REAL_TESTING_TESTING_123",
+		];
 
-	let responses = await Promise.all(
-		usernames.map(username => fetch(`${nowApiBaseUri}/${username}`))
-	);
+		let responses = await Promise.all(
+			usernames.map((username) => fetch(`${nowApiBaseUri}/${username}`))
+		);
 
-	responses = await Promise.all(responses.map(res => res.json()));
+		responses = await Promise.all(responses.map((res) => res.json()));
 
-	responses.forEach(res => expect(res.status).toBe(404));
+		responses.forEach((res) => expect(res.status).toBe(404));
 
-	done();
-});
+		done();
+	});
 
-test("Has CORS enabled", async (done) => {
-	const res = await fetch(nowUri);
-	const corsHeader = res.headers.get("access-control-allow-origin");
-	expect(corsHeader).toBe("*");
-	done();
+	it("has CORS enabled", async (done) => {
+		const sampleUsername = "cristiano";
+		const res = await fetch(`${nowApiBaseUri}/${sampleUsername}`);
+		const corsHeader = res.headers.get("access-control-allow-origin");
+		expect(corsHeader).toBe("*");
+		done();
+	});
 });
